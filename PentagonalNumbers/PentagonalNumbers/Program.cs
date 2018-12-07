c83e26637f444a92c1460c39e0b0bd1648893dec
Alvaro Lupercio <lupercio0184@outlook.com>
2018-12-06 21:14:20 -0500

    Finished method exercise Pentagonal Numbers

---
+++ PentagonalNumbers/PentagonalNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PentagonalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call method to get pentagonal number
+            // return the first 100 pentagonal numbers
+            // with 10 numbers per line
+            for (int i = 1; i < 101; i++)
+            {
+                int pent = getPentagonalNumber(i);
+                Console.Write(pent + "\t");
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+        public static int getPentagonalNumber(int n)
+        {
+            // formula n(3n - 1) / 2
+            int result = n * (3 * n - 1) / 2;
+            return result;
+        }
+    }
+}
