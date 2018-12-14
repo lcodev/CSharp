00ba1c3c588a9ca8eebb73d61cda0a6de6a024b5
Lupercio <lupera1@jhuapl.edu>
2018-12-14 13:06:12 -0500

    Started array exercises.

---
+++ Arrays/AnalyzingNumbers/AnalyzingNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnalyzingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myList = { 2, 4, 6, 8};
+            foreach (int e in myList)
+            {
+                Console.WriteLine(Math.Pow(e, 2));
+            }
+        }
+    }
+}
